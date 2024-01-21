efbc5e6121d7ceff66127bff31cd9fd48e88d9b7
Pankaj Bora <pankajbora1111@gmail.com>
2024-01-21 09:38:54 +0530

    created app

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from extensions import db
+
+def create_app() :
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////emp.db'
+    register_extensions(app)
+    return app
+
+def register_extensions(app) :
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+
+if __name__ == "__main__" :
+    app = create_app()
+    app.run(debug=True)
